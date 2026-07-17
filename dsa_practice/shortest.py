b793a0791cc29cd38dec13d7cabacd3bd59e3711
pallavi papnai <papnaipallavi672@gmail.com>
2026-07-17 21:23:16 +0530

    smallest no in array

--- dsa_practice/shortest.py
+++ dsa_practice/shortest.py
@@ -1,1 +1,7 @@
 #Find the smallest number in an array
+arr=[3,6,1,8,9,0]
+smallest=arr[0]
+for i in range(len(arr)):
+    if arr[i]<smallest:
+        smallest= arr[i]
+print(f"the smallest value is {smallest}")
