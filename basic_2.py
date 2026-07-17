cebbf3809727108570e745fb5b09af83437d2061
pallavi papnai <papnaipallavi672@gmail.com>
2026-07-17 19:39:22 +0530

    short

---
+++ basic_2.py
@@ -0,0 +1,25 @@
+# day 2 : arithematics and some inbuild libraries for bignners 
+# basic arithmatic operations addition ,substraction , multiplication ,division ,modulas,power 
+a = int(input("enter the no 1 :"))
+b = int(input("enter the no 2 :"))
+c= a+b
+d= a-b
+e= a*b
+f= a/b
+g= a**2
+rem=a%b
+print(f"addition:{c},\nsubtraction:{d} ,\nmultiplication:{e},\ndivision:{f},\npower:{g},\nreminder:{rem}")
+# some math related functions 
+n= float(input(" enter the no:"))
+m= float(input(" enter the no :"))
+result = m*n
+print(result)
+result= round(result)
+print(result)
+result=abs(m)
+print(result)
+result=max(a,b,c,d,e,m,n)
+print(result)
+result=min(m,n)
+print(result)
+#these are some of the basic math related function S
