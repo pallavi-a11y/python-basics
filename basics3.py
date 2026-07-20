9ae48db68a66977d98d2576502309e8ba237740b
pallavi papnai <papnaipallavi672@gmail.com>
2026-07-20 21:24:17 +0530

    basics3

--- basics3.py
+++ basics3.py
@@ -8,3 +8,21 @@ elif 30>temp>20 and not is_raning:
     print("event will be continuing")
 
 #conditional expression 
+# one line shortcut for the if _else statement 
+# we use the one line shortcut writing
+#X if conditon else Y
+num=int(input("enter the number"))
+result= "positive"if num>0 else "negative"
+print(result)
+num =int(input("enter the number"))
+result= "positive"if num>0 else "negative"
+print(result)
+temp= float(input("enter the temprature"))
+result="hot"if temp>25 else"cold"
+print(result)
+a=int(input("enter the no1"))
+b=int(input("enter the no2"))
+max_no=a if a>b else b
+print(max_no)
+min_no=a if a>b else b
+print(min_no)
