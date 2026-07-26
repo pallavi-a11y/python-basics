5cd4e35c92661228e540a1ff10369f595fe4aa72
pallavi papnai <papnaipallavi672@gmail.com>
2026-07-26 21:50:17 +0530

    pattern

--- patterns/pattern5.py
+++ patterns/pattern5.py
@@ -1,13 +1,13 @@
 '''Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
-1
-22
-333
-4444
-55555'''
+*****
+****
+***
+**
+*
+Print the pattern in the function given to you.'''
 n= int(input("enter n :"))
 if 1 <= n <= 100:
     i=1
-    for i in range(1,n+1):
-        for j in range(1,i+1):
-            print(i, end ="" )
-        print("")
+    while  i<=n:
+        print("*" *n)
+        n=n-1
